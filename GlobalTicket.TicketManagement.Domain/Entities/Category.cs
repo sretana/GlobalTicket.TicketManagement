6066e3b095f434baee66b369d024875884ef9c8e
susel.retana <susel.retana@GTM-LAP-012>
2021-09-13 10:41:34 -0600

    Agregar archivos de proyecto.

---
+++ GlobalTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using GlobalTicket.TicketManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalTicket.TicketManagement.Domain.Entities
+{
+    class Category:AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
---
+++ GlobalTicket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using GlobalTicket.TicketManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalTicket.TicketManagement.Domain.Entities
+{
+    class Order : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
