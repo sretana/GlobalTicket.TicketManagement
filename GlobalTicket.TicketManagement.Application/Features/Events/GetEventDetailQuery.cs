f92a1d7090ef9110511f414cc437330ad23137a3
sretana <sretana.developer@gmail.com>
2021-09-14 12:04:31 -0600

    Demo: Writing the Application Logic in the Request Handler - Part 2
    creating environment to implement MediatR and autoMapper

---
+++ GlobalTicket.TicketManagement.Application/Features/Events/GetEventDetailQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventDetailQuery: IRequest<EventDetailVm>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ GlobalTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,40 @@
+﻿using GlobalTicket.TicketManagement.Application.Features.Events;
+using GlobalTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace GlobalTicket.TicketManagement.Application.Profiles
+{
+    public class MappingProfile
+    {
+        public MappingProfile()
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Event, EventListVm>().ReverseMap();
+            });
+
+            IMapper mapper = config.CreateMapper();
+            var source = new Event();
+            var dest = mapper.Map<Event, EventListVm>(source);
+
+
+            var config2 = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Event, EventDetailVm>().ReverseMap();
+            });
+
+            IMapper mapper2 = config2.CreateMapper();
+            var source2 = new Event();
+            var dest2 = mapper2.Map<Event, EventDetailVm>(source2);
+
+            var config3 = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Category, CategoryDto>();
+            });
+
+            IMapper mapper3 = config3.CreateMapper();
+            var source3 = new Event();
+            var dest3 = mapper3.Map<Event, EventDetailVm>(source3);
+        }
+    }
+}
