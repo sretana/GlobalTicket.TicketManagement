e648d7faa2445f10fde1a6a93c99ca6ef4fd3c81
sretana <sretana.developer@gmail.com>
2021-09-17 13:51:09 -0600

    adding controllers and actions to api project

---
+++ GlobalTicket.TicketManagement.Api/Controllers/EventsController.cs
@@ -0,0 +1,70 @@
+﻿using GlobalTicket.TicketManagement.Application.Features.Events;
+using GlobalTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
+using GlobalTicket.TicketManagement.Application.Features.Events.Commands.DeleteEvent;
+using GlobalTicket.TicketManagement.Application.Features.Events.Commands.UpdateEvent;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalTicket.TicketManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public EventsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet(Name ="GetAllEvents")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<List<EventListVm>>> GetAllEvents()
+        {
+            var result = await _mediator.Send(new GetEventsListQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}", Name ="GetEventById")]
+        public async Task<ActionResult<EventDetailVm>> GetEventById(Guid id)
+        {
+            var result = await _mediator.Send(new GetEventDetailQuery { Id = id });
+            return Ok(result);
+        }
+
+        [HttpPost(Name = "AddEvent")]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateEventCommand createEventCommand)
+        {
+            var id = await _mediator.Send(createEventCommand);
+            return Ok(id);
+        }
+
+        [HttpPut(Name ="UpdateEvent")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Update([FromBody] UpdateEventCommand updateEventCommand)
+        {
+            await _mediator.Send(updateEventCommand);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}",Name ="DeleteEvent")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var deleteEventCommand = new DeleteEventCommand() { EventId = id };
+            await _mediator.Send(deleteEventCommand);
+            return NoContent();
+        }
+    }
+}
